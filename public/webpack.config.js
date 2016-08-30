2f6e87bbf765bc293e3e293bfd6cb127fe3fffd2
xiaotao Nie <onlythen@yeah.net>
2016-08-30 14:55:53 +0800

    my vuex app

---
+++ public/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+  
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  }
+}
